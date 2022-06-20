44ae43683f7549ea6e0fbd3d02d6a28bcb0e8570
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-06-20 09:49:10 +0530

    Reverse an array and string

---
+++ Reverse/Approach1.py
@@ -0,0 +1,21 @@
+# Reverse the array or string
+
+'''
+Input: [1,2,3]
+Output: [3,2,1]
+'''
+
+# Approach first -- Using slicing
+arr1 = [1,2,3]
+rev_arr1 = arr1[::-1]
+print(f"Reversed array using slicing: {rev_arr1}")
+
+
+
+
+
+
+
+
+
+
